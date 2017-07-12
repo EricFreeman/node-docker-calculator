67efcb94e3d22e91418592edc743698e12a680f2
Eric Freeman <clownhunter@gmail.com>
2017-07-12 12:35:34 -0600

    Initial commit.

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+app.get('/add/:num1/:num2', (req, res) => {
+  res.send('result: ' + getResult(req, (num1, num2) => num1 + num2));
+});
+app.get('/subtract/:num1/:num2', (req, res) => {
+  res.send('result: ' + getResult(req, (num1, num2) => num1 - num2));
+});
+app.get('/multiply/:num1/:num2', (req, res) => {
+  res.send('result: ' + getResult(req, (num1, num2) => num1 * num2));
+});
+app.get('/divide/:num1/:num2', (req, res) => {
+  res.send('result: ' + getResult(req, (num1, num2) => num1 / num2));
+});
+
+function getResult(req, func) {
+  const { num1, num2 } = req.params;
+  let result = func(parseInt(num1), parseInt(num2));
+  if (isNaN(result)) result = 'What tomfoolery is this?';
+  return result;
+}
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
